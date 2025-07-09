2f70bd8bcbdb1fb32741a2d8a1b397d315f1ddce
damirablahat <damirablahat0@gmail.com>
2025-07-10 01:15:57 +0500

    fix pagination

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -17,7 +17,7 @@ export const BottomNav: React.FC<BottomNavProps> = ({ activeTab, onCreateRequest
     }, []);
 
     return (
-        <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-white border-t shadow-sm flex justify-around items-center py-2 z-50">
+        <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-white border-t shadow-sm flex justify-around items-center py-4 z-50">
             {/* Главная */}
             <Link href="/" className="flex-1 flex justify-center">
                 <Button
